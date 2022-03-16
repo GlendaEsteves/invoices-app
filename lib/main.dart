40e14bfbf2227468f811a7bc8a90994668efcedc
GlendaEsteves <glenda.erodrigues@gmail.com>
2022-03-16 19:23:15 -0300

    Implementação da tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'dart:math' as math;
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const InvoicesApp());
@@ -13,7 +12,8 @@ class InvoicesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Invoices App',
-      theme: new ThemeData(scaffoldBackgroundColor: const Color(0x231F20)),
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color.fromRGBO(30, 33, 57, 100)),
       home: const MyHomePage(title: ''),
     );
   }
@@ -32,53 +32,121 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Color.fromRGBO(73, 78, 110, 100),
-        leading: Image.asset("assets/Slogan.png"),
-        actions: [
-          const VerticalDivider(
-            width: 20,
-            thickness: 1,
-            indent: 0,
-            endIndent: 0,
-            color: Color.fromRGBO(133, 139, 178, 100),
-          ),
-          Padding(
-              padding: const EdgeInsets.only(right: 15),
-              child: GestureDetector(
-                  onTap: null,
-                  child: const SizedBox(
-                      width: 40,
-                      height: 40,
-                      child: CircleAvatar(
-                          backgroundImage: AssetImage("assets/Oval.png")))))
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Color.fromRGBO(73, 78, 110, 100),
+          leading: Image.asset("assets/Slogan.png"),
+          actions: [
+            const VerticalDivider(
+              width: 20,
+              thickness: 1,
+              indent: 0,
+              endIndent: 0,
+              color: Color.fromRGBO(133, 139, 178, 100),
+            ),
             Padding(
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                children: <Widget>[
-                  LayoutBuilder(builder: (context, constraints) {
-                    return Container(
-                      height: 400,
-                      width: 400,
-                      decoration: BoxDecoration(
-                          color: Color.fromRGBO(73, 78, 110, 100),
-                          border: Border.all(),
-                          borderRadius: BorderRadius.all(Radius.circular(20))),
-                    );
-                  })
-                ],
-              ),
-            )
+                padding: const EdgeInsets.only(right: 15),
+                child: GestureDetector(
+                    onTap: null,
+                    child: const SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: CircleAvatar(
+                            backgroundImage: AssetImage("assets/Oval.png")))))
           ],
         ),
-      ),
-    );
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    children: <Widget>[
+                      LayoutBuilder(builder: (context, constraints) {
+                        return Container(
+                          height: 400,
+                          width: 400,
+                          decoration: BoxDecoration(
+                              color: const Color.fromRGBO(73, 78, 110, 100),
+                              border: Border.all(
+                                  color:
+                                      const Color.fromRGBO(73, 78, 110, 100)),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(20))),
+                          child: Column(children: [
+                            Padding(
+                              padding: const EdgeInsets.all(15),
+                              child: Text(
+                                'Login',
+                                style: GoogleFonts.spartan(
+                                    textStyle: const TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 16),
+                              child: TextFormField(
+                                style: const TextStyle(color: Colors.white),
+                                decoration: const InputDecoration(
+                                    hintStyle: TextStyle(color: Colors.white),
+                                    border: UnderlineInputBorder(),
+                                    hintText: 'Usuário'),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 16),
+                              child: TextFormField(
+                                style: TextStyle(color: Colors.white),
+                                decoration: const InputDecoration(
+                                    hintStyle: TextStyle(color: Colors.white),
+                                    border: UnderlineInputBorder(),
+                                    hintText: 'Senha'),
+                              ),
+                            ),
+                            Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8, vertical: 16),
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Entrar',
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                      primary: Colors.white,
+                                      textStyle: GoogleFonts.spartan(
+                                          fontWeight: FontWeight.bold)),
+                                )),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 16),
+                              child: TextButton(
+                                  style: TextButton.styleFrom(
+                                      textStyle: GoogleFonts.spartan()),
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Cadastre-se',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        decoration: TextDecoration.underline),
+                                  )),
+                            ),
+                          ]),
+                        );
+                      })
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
